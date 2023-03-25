e56859a00015669beea75df3b598b631e6f1a9c5
Lirik <SobyaninKA@yandex.ru>
2023-03-25 15:30:51 +0500

    Начало

---
+++ itog/Program.cs
@@ -0,0 +1,12 @@
+﻿int input(string msg)
+{
+    System.Console.WriteLine($"{msg} >");
+    int value;
+    if(int.TryParse(Console.ReadLine(), out value))
+    {
+        return value;
+    }
+    System.Console.WriteLine("Вы ввели не число");
+    Environment.Exit(1);
+    return 0;
+}
