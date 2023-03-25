0950351e218fff4d94ba9894a57210abf53cb3a0
Lirik <SobyaninKA@yandex.ru>
2023-03-25 19:58:49 +0500

    Создал методы для программы

--- itog/Program.cs
+++ itog/Program.cs
@@ -10,3 +10,36 @@
     Environment.Exit(1);
     return 0;
 }
+
+string[] myArray(int value)
+{
+    string[] array = new string[value];
+    for (int i = 0; i < value; i++)
+    {
+        Console.Write($"Введите {i} эллемент массива: ");
+        array[i] = Console.ReadLine();
+    }
+    return array;
+}
+
+string[] requestArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length < 4)
+        {
+            System.Console.Write($"{array[i]}  ");
+        }
+    }
+    return array;
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]}  ");
+    }
+}
+
+int value = input("Введите размер  массива: ");
